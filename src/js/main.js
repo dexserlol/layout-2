a7b37613bd639990828c1b6b3f7415c282639c26
dexserlol <sefbx252@gmail.com>
2023-03-22 13:06:45 +0200

    deploy

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+/**? */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //   delay: 700,
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      }
+    },
+  });
